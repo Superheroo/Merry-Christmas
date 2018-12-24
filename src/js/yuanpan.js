f48ee90c03a84c53cba8d104337e5183fee5f183
liangyingcong <396790407@qq.com>
2018-12-25 00:38:14 +0800

    :apple:

--- src/js/yuanpan.js
+++ src/js/yuanpan.js
@@ -3,56 +3,67 @@ var wyaward = "";
 var localshang = localStorage.getItem("keyshang"); //获取本地数据a
 var loaclaward = localStorage.getItem("keyaward"); //获取本地数据keyaward
 var aa = "";
-$("#lotteryBtn").click(function() {
+var localshang=1;
+$("#lotteryBtn").click(function () {
     console.log(localshang)
     localshang++
-    if (localshang == 1 || shang == 1) {
+    if (localshang == 2 || shang == 2) {
         mToast("您还有最后1次机会");
-        var num = parseInt(Math.random() * (100 - 0 + 0) + 0);
+        var num = parseInt(Math.random() * 50+50);
+        console.log(num)
         if (num >= 0 && num < 50) {
             rotateFunc(1, -110, "二等奖 800元"); //
             var award2 = "二等奖 800元";
             wyaward = award2;
-            $('.img100').show();
-            $('.img18').hide();
+            $('.thanks').show();
+            $('.beizi').hide();
+            $('.kouhong').hide();
         }
-        if (num >= 50 && num < 100) {
+        // num = num + 50
+        // num = parseInt(Math.random() * 50+50);
+        if (num >= 50) {
             rotateFunc(1, -20, "海参 5头"); //
             var award2 = "海参 5头";
             wyaward = award2;
-            $('.img100').hide();
-            $('.img18').show();
+            $('.beizi').show();
+            // $('.kouhong').show();
         }
         localStorage.setItem("keyaward", wyaward);
-        setTimeout(function() {
+        setTimeout(function () {
             alertshow1();
         }, 3000)
-    } else if (localshang > 1 || shang > 1) {
+    } else {
         mToast("机会已经用完了");
         return false;
-    } else {
-        var num = parseInt(Math.random() * (100 - 0 + 0) + 0);
-        // console.log(num);
-        if (num >= 0 && num < 50) {
-            rotateFunc(1, -110, "二等奖 800元"); //
-            var award2 = "二等奖 800元";
-            wyaward = award2;
-            $('.img100').show();
-            $('.img18').hide();
-        }
-        if (num >= 50 && num < 100) {
-            rotateFunc(1, -20, "海参 5头"); //
-            var award2 = "海参 5头";
-            wyaward = award2;
-            $('.img100').hide();
-            $('.img18').show();
-        }
-        setTimeout(function() {
-            alertshow1();
-        }, 3000)
     }
+
+    // else if (localshang > 11 || shang > 11) {
+    //     mToast("机会已经用完了");
+    //     return false;
+    // } 
+    // else {
+    //     var num = parseInt(Math.random() * (100 - 0 + 0) + 0);
+    //     console.log(num);
+    //     if (num >= 0 && num < 50) {
+    //         rotateFunc(1, -110, "二等奖 800元"); //
+    //         var award2 = "二等奖 800元";
+    //         wyaward = award2;
+    //         $('.beizi').show();
+    //         $('.kouhong').hide();
+    //     }
+    //     if (num >= 50 && num < 100) {
+    //         rotateFunc(1, -20, "海参 5头"); //
+    //         var award2 = "海参 5头";
+    //         wyaward = award2;
+    //         $('.beizi').hide();
+    //         $('.kouhong').show();
+    //     }
+    //     setTimeout(function() {
+    //         alertshow1();
+    //     }, 3000)
+    // }
 });
-$('#fBtn1').click(function() {
+$('#fBtn1').click(function () {
     var cjUsername = $('#inputmy2').val(); //姓名
     var cjPhone5 = $('#tell2').val(); //电话
     var username = /^[\u4E00-\u9FA5A-Za-z]+$/;
@@ -81,32 +92,32 @@ $('#fBtn1').click(function() {
             value: wyaward
         }];
         data1.jsonval = form;
-		console.log(data1.jsonval)
+        console.log(data1.jsonval)
         $.ajax({
             url: 'http://order.kowadon.com/index.php?a=form',
-			type: 'post',
+            type: 'post',
             jsonp: 'callback',
             jsonpCallback: 'callback_method',
-            data: "name="+data1.jsonval[0].value+"&mobile="+data1.jsonval[1].value+"&content="+data1.jsonval[2].value,
-            success: function(response) {
+            data: "name=" + data1.jsonval[0].value + "&mobile=" + data1.jsonval[1].value + "&content=" + data1.jsonval[2].value,
+            success: function (response) {
                 mToast("领取成功");
-				$(".input1").val('');
-				$(".input2").val('');
-				$('.boxform').css('display','none');
-				$('.backBg').css('display','none');
+                $(".input1").val('');
+                $(".input2").val('');
+                $('.boxform').css('display', 'none');
+                $('.backBg').css('display', 'none');
             }
         });
     }
 })
-var rotateFunc = function(awards, angle, text) { //awards:奖项，angle:奖项对应的角度
+var rotateFunc = function (awards, angle, text) { //awards:奖项，angle:奖项对应的角度
     shang++;
     localStorage.setItem("keyshang", shang);
     $('#yuanpan').stopRotate();
     $("#yuanpan").rotate({
         angle: 0,
         duration: 4000,
-        animateTo: angle + 1440, //angle是图片上各奖项对应的角度，1440是我要让指针旋转4圈。所以最后的结束的角度就是这样子^^
-        callback: function() {}
+        animateTo: angle + 1530, //angle是图片上各奖项对应的角度，1440是我要让指针旋转4圈。所以最后的结束的角度就是这样子^^
+        callback: function () { }
     });
 };
 
@@ -115,15 +126,15 @@ function alertshow1() {
     $('.backBg').show();
     $('.box01').show();
 }
-$('.clodeImg').click(function() {
-    if($('#inputmy2').val()!=""&&$('#tell2').val()!=""){
+$('.clodeImg').click(function () {
+    if ($('#inputmy2').val() != "" && $('#tell2').val() != "") {
         $('.backBg').hide();
         $('.box01').hide();
-    }else{
-        mToast("填写内容不能为空");   
-    }   
+    } else {
+        mToast("填写内容不能为空");
+    }
 })
-$('.close5').click(function() {
+$('.close5').click(function () {
     $('.backBg').hide();
     $('.guaka5').hide();
 })
